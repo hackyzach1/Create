a86e5224ce68acf05b71ad6edb6a71ad60f385b7
hackyzach1 <zachary.mccully@d11.org>
2024-04-21 19:53:01 -0600

    completed cross, but corners is only partially completed,.

--- rubiksCornersDialog.py
+++ rubiksCornersDialog.py
@@ -1,3 +1,8 @@
-from rubiksCrossDialog import favoriteColor
-def rubiksCornersDialog(favoriteColor):
+def rubiksCornersDialog():
+    orientation = input("Look for corner piece on the bottom layer (should be a piece with the color you chose in the last step) and align it under the spot where it's supposed to go. Hint: look at center pieces. A Corner      always goes in between each center. Say 'done' to move onto the next step, or 'help' to learn more.  ")
+    if orientation == "help":
+        print("Check out the sergs B on youtube for some detailed, helpful instructions. ")
+    miniAlgorithm = input("Once you've completed the previous step,  ")
+rubiksCornersDialog()
+
     
